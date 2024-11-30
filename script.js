3cac4eba2e361fb01d03671a533c2de1bb840f69
unknown <salman60mahmud>
2024-11-30 20:32:37 +0600

    second commit

--- script.js
+++ script.js
@@ -1,141 +1,142 @@
-// // Problem-01: 
+// Problem-01: 
 
 
-// function fixWeightMachine(number) {
-//   if (!Array.isArray(number)) {
-//     return "Invalid Input";
-//   }
+function fixWeightMachine(number) {
+  if (!Array.isArray(number)) {
+    return "Invalid Input";
+  }
 
-//   var fixWeights = [];
+  var fixWeights = [];
 
-//   for (var i = 0; i < number.length; i++) {
-//     var weight = number[i];
+  for (var i = 0; i < number.length; i++) {
+    var weight = number[i];
 
-//     if (weight > 0 && typeof weight === "number") {
-//       fixWeights.push(weight);
-//     }
-//   }
+    if (weight > 0 && typeof weight === "number") {
+      fixWeights.push(weight);
+    }
+  }
 
-//   return fixWeights;
-// }
-
-
-
-
-// // Problem-02:
-
-
-// function countVowels(count) {
-//     if (typeof count !== 'string') {
-//         return "Invalid Input";
-//     }
-
-//     let vowelCount = 0;
-//     var vowels = "aeiouAEIOU";
-
-//     for (let i = 0; i < count.length; i++) {
-//         let check = count[i];
-
-//         if (vowels.includes(check)) {
-//             vowelCount++;
-//         }
-//     }
-
-//     return vowelCount;
-// }
+  return fixWeights;
+}
 
 
 
 
 
 
+// Problem-02:
 
 
-// // Problem-03:
+function countVowels(count) {
+    if (typeof count !== 'string') {
+        return "Invalid Input";
+    }
 
+    let vowelCount = 0;
+    var vowels = "aeiouAEIOU";
 
-// function evaluateResult(marks) {
-//   if (!Array.isArray(marks) && typeof mark !== 'number' ) {
-//     return "Invalid Input";
-//   }
+    for (let i = 0; i < count.length; i++) {
+        let check = count[i];
 
-//   var grades = [];
+        if (vowels.includes(check)) {
+            vowelCount++;
+        }
+    }
 
-//   for (var i = 0; i < marks.length; i++) {
-//     var mark = marks[i];
-
-//     if (mark < 50) {
-//       grades.push("F");
-//     }
-//     else if (mark >= 50 && mark <= 59) {
-//       grades.push("D");
-//     }
-//     else if (mark >= 60 && mark <= 69) {
-//       grades.push("C");
-//     }
-//     else if (mark >= 70 && mark <= 79) {
-//       grades.push("B");
-//     }
-//     else if (mark >= 80 && mark <= 89) {
-//       grades.push("A");
-//     }
-//     else if (mark >= 90) {
-//       grades.push("A+");
-//     }
-//     else {
-//       grades.push("invalid");
-//     }
-//   }
-
-//   return grades;
-// }
-
-// console.log(evaluateResult([0, -56, 'right', 67]));
+    return vowelCount;
+}
 
 
 
 
 
-// // Problem-04:
 
 
-// function createPerfectPassword(user) {
-//   if (typeof user !== 'object') {
-//     return "Invalid Input";
-//   }
 
-//   if (user.year.toString().length !== 4) {
-//     return "Invalid Input";
-//   }
+// Problem-03:
 
-//   let password = user.name[0].toUpperCase().concat(user.name.slice(1)).concat(user.year);
-//   return password;
-// }
+
+function evaluateResult(marks) {
+  if (!Array.isArray(marks) && typeof marks !== 'number' ) {
+    return "Invalid Input";
+  }
+
+  var grades = [];
+
+  for (var i = 0; i < marks.length; i++) {
+    var mark = marks[i];
+
+    if (mark < 50) {
+      grades.push("F");
+    }
+    else if (mark >= 50 && mark <= 59) {
+      grades.push("D");
+    }
+    else if (mark >= 60 && mark <= 69) {
+      grades.push("C");
+    }
+    else if (mark >= 70 && mark <= 79) {
+      grades.push("B");
+    }
+    else if (mark >= 80 && mark <= 89) {
+      grades.push("A");
+    }
+    else if (mark >= 90) {
+      grades.push("A+");
+    }
+    else {
+      grades.push("invalid");
+    }
+  }
+
+  return grades;
+}
+
+
+
+
+
+// Problem-04:
+
+
+function createPerfectPassword(user) {
+  if (typeof user !== 'object') {
+    return "Invalid Input";
+  }
+
+  if (user.year.toString().length !== 4) {
+    return "Invalid Input";
+  }
+
+  let password = user.name[0].toUpperCase().concat(user.name.slice(1)).concat(user.year);
+  return password;
+}
 
  
 
 
 
 
-// // Problem-05:
+// Problem-05:
 
 
-// function calculateMonthlyExpenses(earnings, expenses) {
-//   if (!Array.isArray(earnings) || typeof expenses !== 'number') {
-//     return "Invalid Input";
-//   }
+function calculateMonthlyExpenses(earnings, expenses) {
+  if (!Array.isArray(earnings) || typeof expenses !== 'number') {
+    return "Invalid Input";
+  }
 
-//   let total = 0;
-//   for (let i = 0; i < earnings.length; i++) {
-//     total = total + earnings[i];
-//   }
+  let total = 0;
+  for (let i = 0; i < earnings.length; i++) {
+    total = total + earnings[i];
+  }
 
-//   let savings = total - expenses;
-//   if (savings <= 0) {
-//     return "Save more money";
-//   }
+  let savings = total - expenses;
+  if (savings <= 0) {
+    return "Save more money";
+  }
 
-//   return savings;
-// }
+  return savings;
+}
+
 
 
